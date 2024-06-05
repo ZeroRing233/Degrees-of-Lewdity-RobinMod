6df20b62a519ba7204d6c1e3a097ea405fecb2cf
ZeroRing233 <zeroring233@gmail.com>
2024-06-05 17:32:24 +0800

    海边游泳,修改

--- game/javaScript/robinModUtil.js
+++ game/javaScript/robinModUtil.js
@@ -12,3 +12,40 @@ function getRandomValueFromArray(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 window.getRandomValueFromArray = getRandomValueFromArray;
+
+// 当前衣柜里是否有泳装，先判断上装和内衣上装，再判断下装和内衣下装，不要吐槽写法
+function hasSwimmingSuit() {
+    let hasUpper, hasUnderUpper = false;
+    for (let upper of V.wardrobe['upper']) {
+        if (upper.type.includes("swim") && typeof upper.outfitPrimary != 'undefined') {
+            return true;
+        }
+        if (upper.type.includes("swim") && typeof upper.outfitPrimary === 'undefined') {
+            hasUpper = true;
+        }
+    }
+    for (let under_upper of V.wardrobe['under_upper']) {
+        if (under_upper.type.includes("swim") && typeof under_upper.outfitPrimary != 'undefined') {
+            return true;
+        }
+        if (under_upper.type.includes("swim") && typeof under_upper.outfitPrimary === 'undefined') {
+            hasUnderUpper = true;
+        }
+    }
+    if (hasUpper) {
+        for (let lower of V.wardrobe['lower']) {
+            if (lower.type.includes("swim")) {
+                return true;
+            }
+        }
+    }
+    if (hasUnderUpper) {
+        for (let under_lower of V.wardrobe['under_lower']) {
+            if (under_lower.type.includes("swim")) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+window.hasSwimmingSuit = hasSwimmingSuit;
