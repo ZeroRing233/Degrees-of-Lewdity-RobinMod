e3b34bb2cba570997f2f1939fc42e1ae4b4101c8
ZeroRing233 <zeroring233@gmail.com>
2024-07-20 17:38:46 +0800

    获取可食用的鱼鱼

--- game/javaScript/terraiaUtil.js
+++ game/javaScript/terraiaUtil.js
@@ -7,19 +7,19 @@ function hasTerrariaInfo() {
 }
 window.hasTerrariaInfo = hasTerrariaInfo;
 
-// 待和作者讨论
-function hasEatableFish() {
+function getEatableFishList() {
     // 三文鱼，鲈鱼，金枪鱼，红鲷鱼，鳟鱼, 装甲洞穴鱼, 镜面鱼，偏口鱼, 蜂蜜鱼，双鳍鳕鱼，斑驳油鱼，霓虹脂鲤，雀鲷
-    let eatable_fish_list = ['Salmon', 'Bass', 'Tuna', "Red_Snapper", "Trout",
-            "Armored_Cavefish", "Specular_Fish", "Flounder", "Honeyfin", "Double_Cod", "Variegated_Lardfish", "Neon_Tetra", "Damselfish"
-        ]
-        // 岩石龙虾，绿波泥鳅
-    let eatable_seafood_list = ["Rock_Lobster", "Greenwave_Loach"]
-    for (fish of eatable_fish_list) {
+    let all_eatable_fish_list = ['Salmon', 'Bass', 'Tuna', "Red_Snapper", "Trout",
+        "Armored_Cavefish", "Specular_Fish", "Flounder", "Honeyfin", "Double_Cod", "Variegated_Lardfish", "Neon_Tetra", "Damselfish"
+    ]
+    let eatable_fish_list = [];
+    // 岩石龙虾，绿波泥鳅
+    //let all_eatable_seafood_list = ["Rock_Lobster", "Greenwave_Loach"]
+    for (let fish of eatable_fish_list) {
         if (typeof V[fish] === 'number' && V[fish] >= 1) {
-            return true;
+            eatable_fish_list.pushUnique(fish);
         }
     }
-    return false;
+    return eatable_fish_list;
 }
-window.hasEatableFish = hasEatableFish;
+window.getEatableFishList = getEatableFishList;
