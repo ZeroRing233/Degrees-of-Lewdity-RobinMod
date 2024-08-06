fe3d3adee1faa12c9c216bebb3aad828ec036013
ZeroRing233 <zeroring233@gmail.com>
2024-08-06 18:38:47 +0800

    罗宾便条,设置

--- game/javaScript/orphanagePoster.js
+++ game/javaScript/orphanagePoster.js
@@ -9,3 +9,15 @@ function showPostClicked() {
     }
 }
 window.showPostClicked = showPostClicked;
+
+function showRobinNoteClicked() {
+    let value = $('input[name="showRobinNote"]:checked').val();
+    if (value === "showRobinNote") {
+        V.options.showRobinNote = true;
+        $('.robinNoteSetting').show();
+    } else {
+        V.options.showPoster = false;
+        $('.robinNoteSetting').hide();
+    }
+}
+window.showRobinNoteClicked = showRobinNoteClicked;
