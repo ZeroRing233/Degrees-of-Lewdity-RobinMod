c137013878b81aefa0faad1f430e8718fc0efc2f
ZeroRing233 <zeroring233@gmail.com>
2024-08-07 17:32:42 +0800

    搜索框功能(不容易啊)

--- game/javaScript/orphanagePoster.js
+++ game/javaScript/orphanagePoster.js
@@ -34,3 +34,48 @@ function robinModHintClicked() {
     $.wiki("<<overlayReplace \"robinModHint\">>");
 }
 window.robinModHintClicked = robinModHintClicked;
+
+function robinModSearchButtonClicked() {
+    // 点击搜索前，先清空之前的文本高亮
+    robinModClearButtonClicked();
+    let value = T.robinModHintTextbox;
+    if (!value || value.trim() === "") {
+        return;
+    }
+    value = value.trim();
+    let regex = new RegExp(value, 'g');
+    let prehtml = document.getElementById("robinModHintContent").innerHTML;
+    let newHtml = prehtml.replace(regex, "<span class='gold searchResult'>" + value + '</span>');
+    document.getElementById("robinModHintContent").innerHTML = newHtml;
+    let el = document.getElementsByClassName('searchResult');
+    if (el.length > 0) {
+        el[0].scrollIntoView();
+    } else {
+        let newElement = document.createElement('span');
+        newElement.style.color = "gold";
+        newElement.id = "noSearchResult";
+        let newContent = document.createTextNode("无结果");
+        // 添加文本节点 到这个新的 div 元素
+        newElement.appendChild(newContent);
+        let targetElement = document.getElementById('robinModHintContent');
+        insertBefore(newElement, targetElement);
+    }
+
+}
+window.robinModSearchButtonClicked = robinModSearchButtonClicked;
+
+// 清空文本高亮
+function robinModClearButtonClicked() {
+    if (document.getElementById("noSearchResult")) {
+        $("#noSearchResult").hide();
+    }
+    let prehtml = document.getElementById("robinModHintContent").innerHTML;
+    let newHtml = prehtml.replace(/(<\/?span.*?>)/gi, '');
+    document.getElementById("robinModHintContent").innerHTML = newHtml;
+}
+window.robinModClearButtonClicked = robinModClearButtonClicked;
+
+// 在目标元素前面插入新元素
+function insertBefore(newElement, targetElement) {
+    targetElement.parentNode.insertBefore(newElement, targetElement);
+}
