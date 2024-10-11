562866972edfdf192ef1827750537be457d93e69
ZeroRing233 <zeroring233@gmail.com>
2024-10-11 23:34:44 +0800

    上传图片·,第一步

--- game/javaScript/robinModSettings.js
+++ game/javaScript/robinModSettings.js
@@ -8,57 +8,17 @@ $(document).on(":oncloseoverlay", () => {
     Renderer.refresh(Renderer.lastModel);
 });
 
-function loadImg_test() {
-    var files = $('#imgs')[0].files;
-
-    var readFile = function(file) {
-        return new Promise(function(resolve, reject) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                resolve(e.target.result);
-            }
-            reader.readAsDataURL(file);
-        })
-    }
-
-    var gen = async function() {
-        var f, img;
-        for (var i = 0, l = files.length; i < l; i++) {
-            f = await readFile(files[i]);
-            img = document.createElement('img');
-            img.src = f;
-            $('#imgDiv').append(img);
-        }
-    }
-    gen();
-}
-
-async function show_image() {
+function show_image() {
     //首先获取到文件输入框和img元素
     let file_input = document.getElementById("file_input");
     let show_img = document.getElementById("show_img");
     let file = file_input.files[0];
-    let readFile = function(file) {
-        return new Promise(function(resolve, reject) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                alert("我觉得我还可以抢救一下" + e.target.result);
-                resolve(e.target.result);
-            }
-            reader.readAsDataURL(file);
-        })
-    }
-    let gen = async function() {
-        // alert('这个能执行，真的能')
-        f = await readFile(file);
-        alert("获取到的f是" + f);
-        show_img.src = f;
+    let reader = new FileReader();
+    reader.onload = function(e) {
+        // alert("我觉得我还可以抢救一下" + e.target.result);
+        show_img.src = e.target.result;
         show_img.style.display = 'block';
-    }
-    gen();
-    //创建URL对象
-    //show_img.src = window.URL.createObjectURL(file_input.files[0]);
-    //显示图片
-    //show_img.style.display = 'block';
+    };
+    reader.readAsDataURL(file);
 }
 window.show_image = show_image;
