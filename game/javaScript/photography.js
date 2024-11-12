64896a3a2f47d46a2f5a321b3fde483f134dd213
ZeroRing233 <zeroring233@gmail.com>
2024-11-12 20:25:36 +0800

    保存拖动

--- game/javaScript/photography.js
+++ game/javaScript/photography.js
@@ -8,6 +8,12 @@ function getPhotographyImageList() {
             imgElement.id = "image-" + index;
             imgElement.src = image;
             imgElement.alt = "相册图片 " + (index + 1);
+            //拖动相关，只能拖动框不能拖图片
+            imgElement.draggable = false;
+            imageContainer.draggable = true;
+            imageContainer.addEventListener('dragstart', handleDragStart_photo);
+            imageContainer.addEventListener('dragover', handleDragOver_photo);
+            imageContainer.addEventListener('drop', handleDrop_photo);
             imgElement.onclick = function() {
                 // 点击图片时执行的代码
                 // 例如：放大图片、显示图片信息等
@@ -23,3 +29,37 @@ function getPhotographyImageList() {
     });
 }
 window.getPhotographyImageList = getPhotographyImageList;
+
+// 原因：要拖动的是框，不是图片
+function handleDragStart_photo(event) {
+    if (event.target.classList.contains('image-container')) {
+        window.draggedImage = event.target;
+    }
+}
+
+function handleDragOver_photo(event) {
+    event.preventDefault(); // 必须阻止默认行为才能触发 drop 事件
+}
+
+function handleDrop_photo(event) {
+    if (!window.draggedImage) {
+        alert("无法获取拖动位置！取消排序");
+        return;
+    }
+    event.preventDefault();
+    const gallery = document.getElementById("gallery");
+    const images = document.querySelectorAll('.image-container');
+    const targetImage = event.target;
+    console.log("targetImage是" + targetImage);
+
+    // 获取拖拽元素和目标元素的索引
+    const draggedIndex = Array.from(images).indexOf(window.draggedImage);
+    const targetIndex = Array.from(images).indexOf(targetImage);
+
+    // 在目标元素之前插入拖拽元素
+    if (draggedIndex < targetIndex) {
+        gallery.insertBefore(window.draggedImage, targetImage.nextSibling);
+    } else {
+        gallery.insertBefore(window.draggedImage, targetImage);
+    }
+}
