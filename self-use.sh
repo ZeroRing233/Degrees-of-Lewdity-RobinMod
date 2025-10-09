98cf6e0d3c3d24130f2f25075e20bf32c876a4d3
ZeroRing233 <zeroring233@gmail.com>
2025-10-09 11:03:45 +0800

    好久没提交了(无内容)

---
+++ self-use.sh
@@ -0,0 +1,5 @@
+# 不要吐槽，我也不知道为什么会变成这样
+cd /Users/zeroring/Downloads/dol_mod/DOL-Mod-Created-Helper/mods/Degrees-of-Lewdity-RobinMod
+npx webpack
+node "/Users/zeroring/Downloads/dol_mod/DOL-Custom-Spellbook-Mod/game/insertTools-2/packModZip.js" "boot.json"
+mv "/Users/zeroring/Downloads/dol_mod/DOL-Mod-Created-Helper/mods/Degrees-of-Lewdity-RobinMod/DomRobin.mod.zip" "/Users/zeroring/Downloads/dol_mod/DOL-Custom-Spellbook-Mod/game/mods"
